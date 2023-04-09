8f2a1f8ca858fb548571793a7986f492e999446f
Lucash123321 <45396362+Lucash123321@users.noreply.github.com>
2023-04-09 13:00:39 +0300

    Delete lab5/blog directory

--- lab5/blog/articles/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Article
-
-
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'get_excerpt', 'created_date')
-
-
-admin.site.register(Article, ArticleAdmin)
