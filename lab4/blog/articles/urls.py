f7ca9b7a10176029778a92541f61adc599879597
Artem Usachov <ar.usa4yov@yandex.ru>
2023-04-09 13:00:10 +0300

    lab 5

--- lab4/blog/articles/urls.py
+++ lab4/blog/articles/urls.py
@@ -3,5 +3,5 @@ from .views import archive, get_article
 
 urlpatterns = [
     path('', archive),
-    re_path(r'^article/(?P<article_id>\d+)$', get_article, name='get_article')
+    re_path(r'^article/(?P<article_id>\d+)$', get_article)
     ]
